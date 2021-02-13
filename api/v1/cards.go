2ae108b888cb8b6a4f7eb828e69e9fac8b3fdc2b
Brent Pappas <pappasbrent@gmail.com>
2021-02-13 13:40:43 -0500

    Query by name

--- api/v1/cards.go
+++ api/v1/cards.go
@@ -3,8 +3,6 @@ package v1
 import (
 	"cfv-api/constants"
 	"cfv-api/models"
-	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -12,11 +10,11 @@ import (
 
 func GetCards(c *gin.Context) {
 	db := c.MustGet(constants.DB).(*gorm.DB)
-	if _, err := strconv.ParseInt(c.Query("id"), 10, 64); err != nil {
-		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": "invalid query parameter"})
-	} else {
-		cards := []models.Card{}
-		db.Preload("Sets").Preload("TournamentStatuses").Where("id < 43 AND id > 41").Find(&cards)
-		c.JSON(200, cards)
-	}
+	name := c.Query("name")
+	cards := []models.Card{}
+	db.Preload("Sets").
+		Preload("TournamentStatuses").
+		Where(`name = ?`, name).
+		Find(&cards)
+	c.JSON(200, cards)
 }
